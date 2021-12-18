47122bdf676aa7a16a61cc3c6abbd9066d531225
Andrew Coviello <78008163+Vell-ow@users.noreply.github.com>
2021-12-17 21:31:07 -0800

    Add files via upload

---
+++ main (3).cpp
@@ -0,0 +1,14 @@
+// Then, use five cout statements to create ASCII footprints. 
+// Author: Andrew Coviello
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "\n" << endl;
+  cout << "oOOO0  " << endl;
+  cout << "(   ) 0OOOo " << endl;
+  cout << " \\ (  (   ) " << endl;
+  cout << "  \\_)  ) / " << endl;
+  cout << "      (_/ \n" << endl;
+}
